6749f408622f325e2910b0a9038643da0c4d220e
QuentinJanuel <quentinjanuelkij@gmail.com>
2018-08-11 16:35:13 +0200

    Upgrade

--- test/main.js
+++ test/main.js
@@ -1,7 +1,4 @@
-import Screen from "/screen.js";
-import Controllers from "/controllers.js";
-
-let ang = 0;
+let ang = 0, ang2 = 0;
 
 Screen.loop = () => {
 	Screen.clearAll();
@@ -9,16 +6,31 @@ Screen.loop = () => {
 	Screen.drawRect(0, 0, 100, 100);
 	Screen.setColor("#F00");
 	Screen.drawCircle(300, 1000, 300);
-	Screen.drawText(500, 200, "Hello", {
+	Screen.drawText(500, 200, "WE ARE THE BEST", {
 		font: "Comic Sans MS",
-		size: 100,
-		align: "center"
+		size: 100
 	});
-	ang += 0.01;
-	Screen.drawImage(Screen.dimensions.width/2, Screen.dimensions.height/2, "sprite", {
-		xscale: 1,
+	//
+	if(Controllers.length > 0){
+		const angle = Controllers[0].joystick.left.angle;
+		if(angle != 0)
+			ang = -(angle-270)*Math.PI/90;
+	}
+	if(Controllers.length > 1){
+		const angle = Controllers[1].joystick.left.angle;
+		if(angle != 0)
+			ang2 = -(angle-270)*Math.PI/90;
+	}
+	//
+	Screen.drawImage(Screen.dimensions.width/3, Screen.dimensions.height/2, "sprite", {
+		xscale: 0.5,
 		yscale: 0.5,
 		angle: ang
+	});
+	Screen.drawImage(Screen.dimensions.width*2/3, Screen.dimensions.height/2, "sprite", {
+		xscale: 0.5,
+		yscale: 0.5,
+		angle: ang2
 	});
 	Screen.drawImage(100, 100, "icon");
 }
